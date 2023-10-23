b45f6d1750b0e88793dde60a571602a4bbba50dd
erikarrona <erikarrona@outlook.com>
2023-10-23 10:30:01 -0500

    Uploading Current App Version

--- app/src/androidTest/java/com/cosc4319/adapti_project/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/cosc4319/adapti_project/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.group6.adapti;
+package com.cosc4319.adapti_project;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.group6.adapti", appContext.getPackageName());
+        assertEquals("com.cosc4319.adapti_project", appContext.getPackageName());
     }
 }
---
+++ app/src/main/java/com/cosc4319/adapti_project/MainActivity.java
@@ -0,0 +1,34 @@
+package com.cosc4319.adapti_project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView profileName, profileEmail, profileUsername, profilePassword;
+    TextView titleName, titleUsername;
+    Button editProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+
+
+
+    }
+
+}
