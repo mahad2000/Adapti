11b8b1f41ed00a14dbddd4936a7f00be2e7b4a72
Erik Arrona <100013175+erikarrona@users.noreply.github.com>
2023-11-03 16:02:54 -0500

    Worked on Adding Event Fragment
    Continued work on Adding Event Fragment, need the functionality to add an event before I can start making the event viewers.

--- app/src/main/java/com/cosc4319/adapti_project/ui/add_event/AddEventFragment.java
+++ app/src/main/java/com/cosc4319/adapti_project/ui/add_event/AddEventFragment.java
@@ -1,37 +1,79 @@
 package com.cosc4319.adapti_project.ui.add_event;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.cosc4319.adapti_project.DatePickerFragment;
+import com.cosc4319.adapti_project.LoginActivity;
+import com.cosc4319.adapti_project.R;
+import com.cosc4319.adapti_project.SignupActivity;
 import com.cosc4319.adapti_project.databinding.FragmentAddEventBinding;
 
-public class AddEventFragment extends Fragment {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class AddEventFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
 
     private FragmentAddEventBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        AddEventViewModel addEventViewModel =
-                new ViewModelProvider(this).get(AddEventViewModel.class);
 
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        AddEventViewModel addEventViewModel = new ViewModelProvider(this).get(AddEventViewModel.class);
         binding = FragmentAddEventBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.signupButton;
+        final TextView textView = binding.newEventName;
         addEventViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        TextView newEventDate = root.findViewById(R.id.new_event_date);
+        newEventDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getActivity().getSupportFragmentManager(), "date picker");
+            }
+        });
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance().format(calendar.getTime());
+
+        TextView newEventDate = getView().findViewById(R.id.new_event_date);
+        newEventDate.setText(currentDateString);
+    }
+
+
+    // Inside your activity or fragment
+
+
+
+
+
 }
