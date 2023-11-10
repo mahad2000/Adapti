80546cf30c6edca6413497c7453101c0bd6573e0
erikarrona <erikarrona@outlook.com>
2023-11-10 10:07:32 -0600

    Changed the Event Helper to add under the right user for new events

--- app/src/main/java/com/cosc4319/adapti_project/ui/add_event/EventHelper.java
+++ app/src/main/java/com/cosc4319/adapti_project/ui/add_event/EventHelper.java
@@ -1,10 +1,13 @@
 package com.cosc4319.adapti_project.ui.add_event;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class EventHelper {
     private DatabaseReference eventDatabase;
+    private FirebaseAuth firebaseAuth;
 
     public static class Event {
         private String eventTitle;
@@ -40,22 +43,40 @@ public class EventHelper {
     public EventHelper() {
         // Initialize Firebase Database reference
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        eventDatabase = firebaseDatabase.getReference("events");
+        eventDatabase = firebaseDatabase.getReference("users"); // Reference to 'users' node
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     public void addEvent(String eventTitle, String eventDate, String eventTime, boolean isAllDay) {
-        // Generate a unique key for the event
-        String eventId = eventDatabase.push().getKey();
-        if (isAllDay) {
-            eventTime = "";
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+
+            // Reference to the user's events under their unique ID
+            DatabaseReference userEventsRef = FirebaseDatabase.getInstance().getReference("users")
+                    .child(userId)
+                    .child("events")
+                    .push();
+
+            String eventId = userEventsRef.getKey(); // Getting the unique key for the event
+
+            if (isAllDay) {
+                eventTime = "";
+            }
+
+            // Create an Event object to store the event data
+            Event event = new Event(eventTitle, eventDate, eventTime, isAllDay);
+
+            // Save the event data to the Firebase database under the user's 'events' node
+            userEventsRef.setValue(event);
+        } else {
+            // Handle the case when no user is logged in
         }
-
-        // Create an Event object to store the event data
-        Event event = new Event(eventTitle, eventDate, eventTime, isAllDay);
-
-        // Save the event data to the Firebase database under the unique key
-        eventDatabase.child(eventId).setValue(event);
     }
+
 
     // Define a model class for the event
 
